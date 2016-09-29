a3dda214204a4112108aca9564f262f5bc8a54ea
Johannes la Poutré <tech@tabworld.co>
2016-09-29 15:29:54 +0200

    Use boolean type to be consistent

--- migrations/m160929_113059_alter_notification_table.php
+++ migrations/m160929_113059_alter_notification_table.php
@@ -6,7 +6,7 @@ class m160929_113059_alter_notification_table extends Migration
 {
     public function up()
     {
-        $this->addColumn('{{%notification}}', 'send_email', $this->smallInteger(1)->defaultValue(0));
+        $this->addColumn('{{%notification}}', 'send_email', $this->boolean()->defaultValue(false));
         $this->addColumn('{{%notification}}', 'email_sent', $this->timestamp()->null());
         $this->addColumn('{{%notification}}', 'date_due',   $this->timestamp()->null());
     }
